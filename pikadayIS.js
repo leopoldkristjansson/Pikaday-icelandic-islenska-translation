b1dfa4d19f11e18ff2cc592069036e00d8ced8c5
Leopold Kristjansson <leopold@leopold.is>
2016-09-20 12:14:19 +0200

    Create  pikadayIS.js

---
+++ pikadayIS.js
@@ -0,0 +1,11 @@
+		var i18n = {
+		        previousMonth : 'Fyrri mánuður',
+		        nextMonth     : 'Næsti mánuður',
+		        months        : ['Janúar','Febrúar', 'Mars','Apríl','Maí','Juní','Julí','Ágúst','September','Október','Nóvember','Desember'],
+		        weekdays      : ['Sunnudagur','Mánudagur','Þriðjudagur','Miðvikudagur','Fimmtudagur','Föstudagur','Laugardagur'],
+		        weekdaysShort : ['Sun.', 'Mán.', 'Þri.', 'Mið.', 'Fim.', 'Fös.', 'Lau.']
+		};
+		
+		new Pikaday({
+			i18n: i18n
+		});
